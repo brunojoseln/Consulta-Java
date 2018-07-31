af8d92dbdf1111ef9cef2e02d8f5acab79126a5b
Bruno José <bruno.lima184@etec.sp.gov.br>
2018-07-31 11:19:29 -0300

    Vaŕiáveis estáticas :cookie:

---
+++ Exer/Somador.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+public class Somador {
+    
+    String nome;
+    int valorInstancia = 0;
+    static int valorEstatica = 0;
+    
+    void somar(){
+        valorInstancia++;
+        valorEstatica++;
+    }
+    
+    void imprimir(){
+        System.out.println("O somador nome: "+nome+": instancia: "+valorInstancia+" e estatica: "+valorEstatica);
+    }
+}
